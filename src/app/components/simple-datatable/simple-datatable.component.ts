de69a24d00c267dbe770ed9a1408b6614237717c
Nick Dimizas <nikkosdim@gmail.com>
2025-04-29 02:04:41 +0300

    feat:sorting table

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
+import { sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './simple-datatable.component.css'
 })
 export class SimpleDatatableComponent {
+  @Input() data: EPerson[] | undefined;
 
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age:'none',
+    email:'none',
+    education:'none'
+  }
+
+  sortData(sortKey: keyof EPerson) {
+    console.log(sortKey);
+  }
+
+  onPersonClicked(person:EPerson){
+    console.log("Person>>",person)
+  }
 }
